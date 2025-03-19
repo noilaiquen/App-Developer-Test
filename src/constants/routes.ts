46536d47ac410d7d1ed33b370e770b53fcc62d18
I'm Binh.Nguyen <nguyenthanhbinh255@gmail.com>
2025-03-19 23:31:44 +0700

    Add custom fonts and implement bottom tab navigation

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,3 +1,4 @@
 export const MAIN = 'Main';
+export const BOTTOM_TABS = 'BottomTabs';
 export const HOME_SCREEN = 'HomeScreen';
 export const MOVE_DETAIL_SCREEN = 'MovieDetailScreen';
--- src/navigation/root.tsx
+++ src/navigation/root.tsx
@@ -1,17 +1,20 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
 import {
   NativeStackNavigationOptions,
   createNativeStackNavigator,
 } from 'react-native-screens/native-stack';
-import {HeaderCenter} from '../components';
+import {HeaderCenter, Text, View} from '../components';
 import {BaseColors, useTheme} from '../configs/theme';
 import {ROUTES} from '../constants';
 import HomeScreen from '../screens/home/index';
 import MovieDetail from '../screens/movieDetail/index';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const RootStack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export const RootNavigation = (): JSX.Element => {
+export const RootNavigation = () => {
   const {colors} = useTheme();
 
   const baseOptions: NativeStackNavigationOptions = {
@@ -29,7 +32,7 @@ export const RootNavigation = (): JSX.Element => {
     <RootStack.Navigator
       initialRouteName={ROUTES.HOME_SCREEN}
       screenOptions={baseOptions}>
-      <RootStack.Screen name={ROUTES.HOME_SCREEN} component={HomeScreen} />
+      <RootStack.Screen name={ROUTES.BOTTOM_TABS} component={BottomTabs} />
       <RootStack.Screen
         name={ROUTES.MOVE_DETAIL_SCREEN}
         component={MovieDetail}
@@ -44,5 +47,28 @@ export const RootNavigation = (): JSX.Element => {
         }}
       />
     </RootStack.Navigator>
+  );
+};
+
+export const BottomTabs = (): JSX.Element => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="HomeTab"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: () => <Ionicons name="home" size={30} />,
+          // You can add tabBarIcon here
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={View} // Replace with your Search screen component
+        options={{
+          tabBarLabel: () => <Ionicons name="home" size={30} />,
+          // You can add tabBarIcon here
+        }}
+      />
+    </Tab.Navigator>
   );
 };
